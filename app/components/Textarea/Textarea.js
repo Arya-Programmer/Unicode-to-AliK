aca050d192d1e4c9e966a639bb32d30df596ec75
Arya K. O <67804463+Arya-Programmer@users.noreply.github.com>
2024-07-12 02:03:03 +0300

    Design text area

--- app/components/Textarea/Textarea.js
+++ app/components/Textarea/Textarea.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 import styles from "./textarea.module.css";
 
 function Textarea({ isUnicode }) {
-  const placeholder = isUnicode 
-            ? "ئەو نووسینەی بە کیبۆردی کوردی نووسیوتە یان بە فۆنتێکی یونیکۆد نووسیوتە لێرە دایبنێ تا بیگۆڕین بە ئەلیکەی"
-            : "ئەو نووسینەی بە کیبۆردی عەرەبی نووسیوتە یان بە فۆنتێکی ئەلیکەی نووسیوتە لێرە دایبنێ تا بیگۆڕین بە یونیکۆد"
+  const placeholder = isUnicode
+    ? "ئەو نووسینەی بە کیبۆردی کوردی نووسیوتە یان بە فۆنتێکی یونیکۆد نووسیوتە لێرە دایبنێ تا بیگۆڕین بە ئەلیکەی"
+    : "ئەو نووسینەی بە کیبۆردی عەرەبی نووسیوتە یان بە فۆنتێکی ئەلیکەی نووسیوتە لێرە دایبنێ تا بیگۆڕین بە یونیکۆد";
+  
+  const title = isUnicode ? "Unicode" : "AliK"; 
+
   return (
-    <textarea className={styles.textarea} placeholder={isUnicode 
-        ? "ئەو نووسینەی بە کیبۆردی کوردی نووسیوتە یان بە فۆنتێکی یونیکۆد نووسیوتە لێرە دایبنێ تا بیگۆڕین بە ئەلیکەی"
-        : "ئەو نووسینەی بە کیبۆردی عەرەبی نووسیوتە یان بە فۆنتێکی ئەلیکەی نووسیوتە لێرە دایبنێ تا بیگۆڕین بە یونیکۆد"}/>
+    <span className={styles.textareaContainer}>
+      <div className={styles.textareaTitle}>{title}</div>
+      <textarea className={styles.textarea} placeholder={placeholder}></textarea>
+    </span>
   );
 }
 
